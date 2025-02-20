1525627d4f6d9d26c72e0253fd75e074bc45d3d3
sushreebhol <49037195+sushreebhol@users.noreply.github.com>
2025-02-20 20:56:28 +0100

    Update AwsSecretsManagerConfig.java

--- src/test/java/com/test/practice/config/AwsSecretsManagerConfig.java
+++ src/test/java/com/test/practice/config/AwsSecretsManagerConfig.java
@@ -1,15 +1,26 @@
-import io.awspring.cloud.secretsmanager.SecretsManagerPropertySource;
+package com.test.practice.config;
+
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import io.awspring.cloud.secretsmanager.AwsSecretsManagerPropertySource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertySource;
-import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 
 @Configuration
 public class AwsSecretsManagerConfig {
 
     @Bean
-    public PropertySource<?> awsSecretsManagerPropertySource(Environment environment, SecretsManagerClient secretsManagerClient) {
-        return new SecretsManagerPropertySource(environment, secretsManagerClient);
+    public AWSSecretsManager awsSecretsManager() {
+        return AWSSecretsManagerClientBuilder.standard().build();
+    }
+
+    @Bean
+    public PropertySource<?> awsSecretsManagerPropertySource(Environment environment, AWSSecretsManager awsSecretsManager) {
+        return new AwsSecretsManagerPropertySource("aws-secrets", awsSecretsManager);
     }
 }
+
+
+
